e4ea0dcbb17e8ee8918f879b52dec308d8022e84
lucaskup <LKUPSSINSKU@edu.unisinos.br>
2020-07-11 19:36:13 -0300

    Solved issue with null geometry

--- scripts/fractStats.py
+++ scripts/fractStats.py
@@ -7,21 +7,22 @@ myline = qgis.utils.iface.activeLayer()
 myline.selectByExpression("\"id\">0")
 
 d = QgsDistanceArea()
-
-
-d = QgsDistanceArea()
 d.setEllipsoid('WGS84')
 
 for elem in myline.selectedFeatures():
     id = elem.id()
-    xy = elem.geometry().asMultiPolyline()
-    #calculates the azimuth
-    az = xy[0][0].azimuth(xy[0][-1])
-    #normalize the angle 0 - 180
-    if (az < 0):
-        az = 180 + az
+    #print(elem.geometry())
+    #avoids error for null geometry
+    if not elem.geometry().isNull():
+        xy = elem.geometry().asMultiPolyline()
+        #calculates the azimuth
+        #0 starts at the north and goes clockwise
+        az = xy[0][0].azimuth(xy[0][-1])
+        #normalize the angle 0 - 180
+        if (az < 0):
+            az = 180 + az
+            
+        #length = xy[0][0].sqrDist(xy[0][-1])
+        measure = d.measureLine(xy[0])
         
-    #length = xy[0][0].sqrDist(xy[0][-1])
-    measure = d.measureLine(xy[0])
-    
-    print(id,  az,measure)
+        print(id,  az,measure)
